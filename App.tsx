72a2fa1e1a6ef821580e96dd625b1bbef4bcae6c
ericknathan <erick.capito@hotmail.com>
2021-12-07 16:57:12 -0300

    Implemented form and navigation

--- App.tsx
+++ App.tsx
@@ -9,8 +9,10 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins'
 
-import theme from './src/global/styles/theme'
-import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
     );
 }
